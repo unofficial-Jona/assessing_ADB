4dac0aa1c71c38d2ba536aa32bb1086490e2f3cf
unofficial-jona <jonathan.aechtner@web.de>
2023-02-12 17:13:03 +0000

    updated feature extractor for Swin + GMFlowNet models

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,11 +1,12 @@
-# %%
+# addaptations to 'normal feature extractor to acomodate the GMFlowNet model
+
 import torchvision.transforms.functional as TF
 from torchvision.models.optical_flow import raft_large
 from torchvision.models.optical_flow import Raft_Large_Weights
 from torchvision.utils import flow_to_image
 from torchvision.models.feature_extraction import get_graph_node_names
 from torchvision.models.feature_extraction import create_feature_extractor
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50, ResNet50_Weights, swin_v2_b, Swin_V2_B_Weights
 import numpy as np
 from glob import glob
 from zipfile import ZipFile
@@ -16,13 +17,17 @@ from datetime import datetime
 import warnings
 import gc
 import argparse
+import json
 
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 from torchvision.io import read_video
 import torch
 
-from multiprocessing import cpu_count, Pool, log_to_stderr, Manager
+
+from prepare_feat_ext import model as flow_model
+
+import pdb
 
 # %%
 class FeatureExtractor():
@@ -35,9 +40,11 @@ class FeatureExtractor():
 
         self.resize = kwargs.get('resize', (448,448))
     
-        self.batch_size = kwargs.get('batch_size', 32)
+        self.batch_size = kwargs.get('batch_size', 64)
 
         self.error_file = dict()
+        
+        self.json_file = json.load(open('../../pvc-meteor/features/METEOR_info.json', 'r'))
 
         if 'flow_model' in kwargs.keys():
             warnings.warn('Using a different flow model may require disabling backprop', category=UserWarning, stacklevel=2)
@@ -46,22 +53,24 @@ class FeatureExtractor():
             self.transforms = flow_weights.transforms()
 
         else:
-            self.flow_model = raft_large(
-                weights=Raft_Large_Weights.DEFAULT, progress=False).to(self.device)
+            self.flow_model = flow_model.to(self.device)
             for param in self.flow_model.parameters():
                 param.requires_grad = False
-            flow_weights = Raft_Large_Weights.DEFAULT
-            self.transforms = flow_weights.transforms()
+            self.flow_model.eval()
+            # does not need inference transform, as image size is unimportant for flow estimation
+            
 
         if 'rgb_model' in kwargs.keys():
             self.rgb_model = kwargs['rgb_model']
             warnings.warn('Using a different rgb model may require disabling backprop', category=UserWarning, stacklevel=2)
         else:
-            model = resnet50(weights=ResNet50_Weights.DEFAULT, progress=False)
+            model = swin_v2_b(weights=Swin_V2_B_Weights.DEFAULT, progress=False)
             for param in model.parameters():
                 param.requires_grad = False
-            self.rgb_model = create_feature_extractor(
-                model, return_nodes={'flatten': 'flatten'}).to(self.device)
+            model.eval()
+            self.rgb_model = create_feature_extractor(model, return_nodes={'flatten': 'flatten'}).to(self.device)
+            
+            self.rgb_inference_transform = Swin_V2_B_Weights.DEFAULT.transforms()
 
         if 'vid_dir' in kwargs.keys():
             self.vid_dir = kwargs['vid_dir']
@@ -88,48 +97,50 @@ class FeatureExtractor():
             self.labels_mapping = {'OverTaking': 0, 'OverSpeeding': 1, 'LaneChanging(m)': 2, 'LaneChanging': 2, 'TrafficLight': 3, 'WrongLane': 4, 'WrongTurn': 5, 'Cutting': 6}
 
     def video_processor(self, video):
+        # pdb.set_trace()
+        
         frames, _, _ = read_video(video, pts_unit='sec', output_format='TCHW')
 
-        rgb_frames = torch.stack([frames[i] for i in range(
-            frames.shape[0]) if i % self.frame_interval == 0])
-        rgb_frames = TF.resize(rgb_frames, size=self.resize)
-        # divide by 255 to map pixel values to interval [0,1]
-        rgb_frames = (rgb_frames/255).to(self.device)
-
+        rgb_frames = torch.stack([frames[i] for i in range(frames.shape[0]) if i % self.frame_interval == 0])
+        
+        # use provided inference transform, so images can be passed to the models
+        rgb_frames = self.rgb_inference_transform(rgb_frames).to(self.device)
+        
         # extract rgb_features
         rgb_chunks = torch.split(rgb_frames, split_size_or_sections=self.batch_size, dim=0)
         rgb_features = []
-
+        
+        # pdb.set_trace()
+        
         for chunk in rgb_chunks:
             features = self.rgb_model(chunk)['flatten']
             rgb_features.append(features)
-
-        # gc.collect(features, rgb_chunks, chunk)
+        
         rgb_features = torch.cat(rgb_features, dim=0).to('cpu')
         rgb_features = rgb_features[1:]
-
-        # extract flow_features
-        flow_stack1, flow_stack2 = self.transforms(rgb_frames[:-1], rgb_frames[1:])
+        
+        # extract flow_features      
+        flow_stack1, flow_stack2 = rgb_frames[:-1], rgb_frames[1:]
         flow_stack1 = torch.split(flow_stack1, split_size_or_sections=self.batch_size, dim=0)
         flow_stack2 = torch.split(flow_stack2, split_size_or_sections=self.batch_size, dim=0)
 
         flow_features = []
 
         for stack1, stack2 in zip(flow_stack1, flow_stack2):
-            features = self.flow_model(stack1, stack2)
-            # append last item from extracted flow_features, as it corresponds to last iteration (most accurate one)
+            features, _ = self.flow_model(stack1, stack2)
             features = flow_to_image(features[-1])
-            features = self.rgb_model(features/255)['flatten']
+            
+            features = self.rgb_inference_transform(features)
+            features = self.rgb_model(features)['flatten']
             flow_features.append(features)
 
-        # gc.collect(features, stack1, stack2, flow_stack1, flow_stack2)
         flow_features = torch.cat(flow_features, dim=0).to('cpu')
 
-        # assert flow_features.shape == rgb_features.shape, f'shapes of rgb ({rgb_features.shape}) and flow ({flow_features.shape}) features do not match'
+        rgb_features, flow_features = rgb_features.detach().cpu().resolve_conj().resolve_neg().numpy(), flow_features.detach().cpu().resolve_conj().resolve_neg().numpy()
 
-        return rgb_features.detach().cpu().resolve_conj().resolve_neg().numpy(), flow_features.detach().cpu().resolve_conj().resolve_neg().numpy()
+        return {'rgb':rgb_features, 'flow':flow_features}
 
-    # '/workspace/pvc-meteor/downloads/Video XML Annotations'
+    
     def annotation_processor(self, video_name):
         """generates annotations
 
@@ -144,6 +155,7 @@ class FeatureExtractor():
 
         if zip_name not in os.listdir(self.anno_zip_dir):
             self.error_file[video_name] = 'zip file not found'
+            # print(video_name)
             return None
 
         else:
@@ -214,37 +226,16 @@ class FeatureExtractor():
                         '''
         return {'anno':template[1:], 'feature_length':template[1:].shape[0]}
 
-    def get_dict_for_info_json(self, output_dict, test_split=0.2):
-        """generates dictionary for data_info_new.json as required by OadTR.
+    def split_according_to_json(self, file_to_split):
+        train = {}
+        test = {}
 
-        Args:
-            output_dict (dict, str): dictionary object contaning the key 'annotations' or filepath to such a dictionary.
-            test_split (float, optional): relative share of the data used for evaluation. Defaults to 0.2.
+        for key, dic in zip(['train_session_set', 'test_session_set'], [train, test]): 
 
-        Returns:
-            dictionary: dictionary with the keys ['class_index', 'train_session_set', 'test_session_set']
-        """
-        if isinstance(output_dict, str):
-            with open(output_dict, 'rb') as f:
-                output_dict = pickle.load(f)
-        
-        video_names = list(output_dict['annotations'].keys())
-        train, test = train_test_split(video_names, test_size=test_split)
+            for vid_name in self.json_file['METEOR'][key]:
+                dic.update({vid_name: file_to_split[vid_name]})
 
-        seen_values = []
-        class_index = []
-        for k, v in self.labels_mapping.items():
-            if v in seen_values:
-                continue
-            else:
-                seen_values.append(v)
-                class_index.append(k)
-        
-        return {
-            'class_index': class_index,
-            'train_session_set': train,
-            'test_session_set': test
-            }
+        return train, test
 
 
     def file_processing(self, file_path):
@@ -252,41 +243,38 @@ class FeatureExtractor():
         zip_file_name = video_file_name[:-4] + '.zip'
 
         annotation = self.annotation_processor(zip_file_name)
-
+        # pdb.set_trace()
         # skip video if no annotations are available
-        if not isinstance(annotation, np.ndarray):
+        if not isinstance(annotation['anno'], np.ndarray):
             return None, None
 
         else:
-            rgb_features, flow_features = self.video_processor(
-                os.path.join(self.vid_dir, video_file_name))
+            feature_dict = self.video_processor(os.path.join(self.vid_dir, video_file_name))
 
-        if rgb_features.shape != flow_features.shape:
-            print(f'rgb_shape={rgb_features.shape}, flow_shape={flow_features.shape}')
+        if feature_dict['rgb'].shape != feature_dict['flow'].shape:
             self.error_file[video_file_name] = 'feature shape mismatch'
+            # print(video_file_name)
             return None, None
-        if annotation.shape[0] != rgb_features.shape[0]:
+        if annotation['anno'].shape[0] != feature_dict['rgb'].shape[0]:
             self.error_file[video_file_name] = 'annotation shape mismatch'
+            # print(video_file_name)
             return None, None
 
         # create dictionary and write to pickle file
-        return {
-            video_file_name:
-                {
-                    'rgb': rgb_features,
-                    'flow': flow_features
-                }
-        }, {video_file_name: annotation}
+        return {video_file_name:feature_dict}, {video_file_name: annotation}
 
-    def dir_processing(self, save=True):
-        if use_mp: 
-            warnings.warn('using multiprocessing with cuda is currently not supported', category=UserWarning, stacklevel=2)
-            
+    def dir_processing(self, save=True, prepare=True, file_save=True):
+        if prepare:
+            print('prepare enabled')
+        if file_save:
+            print('file_save enabled')
+        
+        
         # keep start time to differentiate between pickled files
         start_time = datetime.now()
         start_time = start_time.strftime("%d-%m-%Y-%H-%M")
 
-        general_informaion = {
+        general_information = {
             'fps': self.frame_interval, 
             'rgb_extractor': self.rgb_model.__class__.__name__,
             'flow_extractor': self.flow_model.__class__.__name__, 
@@ -308,10 +296,10 @@ class FeatureExtractor():
                 annotation_dict.update(vid_annot)
 
 
-                if save:
-
+                if file_save:
+                    # print('file_save reached')
                     file_name = f'file_features_{file_path[-29:-4]}.pkl'
-                    vid_save = {'meta': general_informaion,
+                    vid_save = {'meta': general_information,
                         'features': vid_features, 
                         'annotations': vid_annot
                         }
@@ -321,6 +309,7 @@ class FeatureExtractor():
 
             except:
                 self.error_file[file_path[-29:-4]] = 'catched by try-except block'
+                # print(file_path[-29:-4])
                 continue
 
         output_dict = {
@@ -333,36 +322,41 @@ class FeatureExtractor():
         if save:
             # create the file name using the start_time and extracted_features variables
             file_name = f'extraction_output_{start_time}.pkl'
-            anno_name = f'annotations_{start_time}.pkl'
-            feature_name = f'annotations_{start_time}.pkl'
-            
-            with open(self.output_dir + anno_name, 'wb') as file:
-                # use pickle to serialize the dictionary and write it to the file
-                pickle.dump(annotation_dict, file)
-
-            with open(self.output_dir + feature_name, 'wb') as file:
-                # use pickle to serialize the dictionary and write it to the file
-                pickle.dump(feature_dict, file)
 
             # open the file in write mode
             with open(self.output_dir + file_name, 'wb') as file:
                 # use pickle to serialize the dictionary and write it to the file
                 pickle.dump(output_dict, file)
-
-            json_dict = self.get_dict_for_info_json(output_dict)
-            with open(self.output_dir + 'data_info_new.json', 'w') as file:
-                json.dump(json_dict, file)
         
-        return output_dict, json_dict
+        
+        if prepare:
+            train, test = self.split_according_to_json(output_dict['features'])
+
+            with open(self.output_dir + 'features_METEOR_train.pickle', 'wb') as f:
+                pickle.dump(train, f)
+
+            with open(self.output_dir + 'features_METEOR_test.pickle', 'wb') as f:
+                pickle.dump(test, f)
+                
+            train, test = split_according_to_json(json_file, output_dict['annotations'])
+            
+            with open(self.output_dir + 'target_METEOR_train.pickle', 'wb') as f:
+                pickle.dump(train, f)
+
+            with open(self.output_dir + 'target_METEOR_test.pickle', 'wb') as f:
+                pickle.dump(test, f)
+
+        
+        return output_dict
 
 
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Extract frames from videos')
-    parser.add_argument('--FPS', type=int)
+    parser.add_argument('--FPS', type=int, default=15)
     config = parser.parse_args()
 
 
     extractor = FeatureExtractor(config.FPS)
-    extractor.dir_to_features()
+    extractor.dir_processing()
